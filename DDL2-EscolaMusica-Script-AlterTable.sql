aaae532eba520924910536e7f5990ba310259cc8
Marcos <marcosprofissional15@gmail.com>
2024-09-28 11:17:27 -0300

    Adicionando Scripts Alter Table

---
+++ DDL2-EscolaMusica-Script-AlterTable.sql
@@ -0,0 +1,43 @@
+-- Adicionar coluna descricao à tabela Orquestra
+ALTER TABLE `escolaMusica`.`Orquestra`
+ADD COLUMN `descricao` VARCHAR(255) NULL AFTER `nome`;
+
+-- Modificar o tipo de dado da coluna nome na tabela Musico
+ALTER TABLE `escolaMusica`.`Musico`
+MODIFY COLUMN `nome` VARCHAR(100) NOT NULL;
+
+-- Renomear a coluna funcao para posicao na tabela Musico
+ALTER TABLE `escolaMusica`.`Musico`
+CHANGE COLUMN `funcao` `posicao` VARCHAR(45) NOT NULL;
+
+-- Adicionar índice único na coluna nome da tabela Orquestra
+ALTER TABLE `escolaMusica`.`Orquestra`
+ADD UNIQUE INDEX `unique_nome` (`nome`);
+
+-- Remover a coluna dataCriacao da tabela Orquestra
+ALTER TABLE `escolaMusica`.`Orquestra`
+DROP COLUMN `dataCriacao`;
+
+-- Adicionar coluna email à tabela Musico
+ALTER TABLE `escolaMusica`.`Musico`
+ADD COLUMN `email` VARCHAR(100) NULL AFTER `nome`;
+
+-- Modificar o tipo de dado da coluna dataNascimento na tabela Musico
+ALTER TABLE `escolaMusica`.`Musico`
+MODIFY COLUMN `dataNascimento` DATETIME NOT NULL;
+
+-- Adicionar chave estrangeira na tabela Musico para referenciar Instrumento
+ALTER TABLE `escolaMusica`.`Musico`
+ADD COLUMN `Instrumento_idInstrumento` INT NOT NULL,
+ADD CONSTRAINT `fk_Musico_Instrumento`
+FOREIGN KEY (`Instrumento_idInstrumento`)
+REFERENCES `escolaMusica`.`Instrumento` (`idInstrumento`)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+-- Renomear a tabela Musico para Musicista
+RENAME TABLE `escolaMusica`.`Musico` TO `escolaMusica`.`Musicista`;
+
+-- Adicionar coluna telefone à tabela Musicista
+ALTER TABLE `escolaMusica`.`Musician`
+ADD COLUMN `telefone` VARCHAR(20) NULL AFTER `email`;
